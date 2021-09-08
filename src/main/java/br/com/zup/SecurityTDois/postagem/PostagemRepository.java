8c51a641d9878e733c487ac5b8323ef00682faf4
Vinícius <vinicius.raymundo@zup.com.br>
2021-09-08 17:02:07 -0300

    Criar repositoria para postagem

---
+++ src/main/java/br/com/zup/SecurityTDois/postagem/PostagemRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.SecurityTDois.postagem;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostagemRepository extends CrudRepository<Postagem, Integer> {
+}
