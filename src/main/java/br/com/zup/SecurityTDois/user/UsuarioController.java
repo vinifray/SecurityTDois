ebad399663dfb67e6d526d27c877b90f5148586e
Vinícius <vinicius.raymundo@zup.com.br>
2021-09-08 14:05:12 -0300

    Adicionar rota para visualizar usuario

--- src/main/java/br/com/zup/SecurityTDois/user/UsuarioController.java
+++ src/main/java/br/com/zup/SecurityTDois/user/UsuarioController.java
@@ -1,10 +1,10 @@
 package br.com.zup.SecurityTDois.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("/usuario")
@@ -13,7 +13,17 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     @PostMapping
-    public Usuario cadastrarUsuario(@RequestBody Usuario usuario){
+    public Usuario cadastrarUsuario(@RequestBody Usuario usuario) {
         return usuarioService.cadastrarUsuario(usuario);
+    }
+
+    @GetMapping("/{idUsuario}")
+    public Usuario pesquisarUsuarioPeloId(@PathVariable(name = "idUsuario") int id) {
+        try {
+            return usuarioService.buscarUsuarioPeloId(id);
+        } catch (RuntimeException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+
     }
 }
