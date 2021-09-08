f470bb2cfea3e5d4d48a03605e064b1db484b32b
Vinícius <vinicius.raymundo@zup.com.br>
2021-09-08 14:01:18 -0300

    Criar controller para usuario

--- src/main/java/br/com/zup/SecurityTDois/user/UsuarioController.java
+++ src/main/java/br/com/zup/SecurityTDois/user/UsuarioController.java
@@ -1,4 +1,14 @@
 package br.com.zup.SecurityTDois.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuario")
 public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+
+
 }
