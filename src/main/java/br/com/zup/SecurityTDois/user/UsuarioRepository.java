9593a0a7bcff5659909ba7404f4689af9c6558ff
Vinícius <vinicius.raymundo@zup.com.br>
2021-09-08 13:52:55 -0300

    Adicionar repositorio para usuario

---
+++ src/main/java/br/com/zup/SecurityTDois/user/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.SecurityTDois.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
+}
